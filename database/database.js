b553dc97d5978378063a0cb05243a57df08a5ca6
Ryan <ryanshaunsmith@gmail.com>
2024-10-21 16:52:05 +0100

    set up database

---
+++ database/database.js
@@ -0,0 +1,34 @@
+import sqlite3 from "sqlite3";
+
+const DBSOURCE = "db.sqlite";
+
+const db = new sqlite3.Database(DBSOURCE, (err) => {
+  if (err) {
+    // Can't open database
+    console.error(err.message);
+    throw err;
+  } else {
+    console.log("Connected to the SQLite database.");
+    db.run(
+      `CREATE TABLE products (
+    stock_number INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    description TEXT,
+    price REAL NOT NULL
+    )`,
+      (err) => {
+        if (err) {
+          // Table already created
+        } else {
+          // Table just created, creating some rows
+          const insert =
+            "INSERT INTO products (name, description, price) VALUES (?,?,?)";
+          db.run(insert, ["product1", "description1", 100]);
+          db.run(insert, ["product2", "description2", 200]);
+        }
+      }
+    );
+  }
+});
+
+export default db;
---
+++ database/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE products (
+    stock_number INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    description TEXT,
+    price REAL NOT NULL
+);
