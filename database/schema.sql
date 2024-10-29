db55de6ea5f8476e5eb1f526fa2083417334a2e2
Ryan <ryanshaunsmith@gmail.com>
2024-10-29 09:26:59 +0000

    render product list from db

--- database/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE products (
-    stock_number INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    description TEXT,
-    price REAL NOT NULL
-);
--- public/script.js
+++ public/script.js
@@ -1,7 +1,34 @@
+const productList = document.querySelector(".product-list");
+
 const getProductData = async () => {
   const response = await fetch("http://localhost:3001/api/products");
   const data = await response.json();
-  console.log(data.rows);
+  return data.rows;
 };
 
-getProductData();
+// !! Clear list
+const clearList = () => {
+  while (productList.firstChild) {
+    productList.removeChild(productList.firstChild);
+  }
+};
+
+// !! Render List Item
+const renderListItem = (product) => {
+  const productName = product.name;
+  const listItem = document.createElement("li");
+  listItem.innerText = productName;
+  return listItem;
+};
+
+// !! Render Product list !!
+const renderProductList = async () => {
+  clearList();
+  const productData = await getProductData();
+  productData.map((product) => {
+    const listItem = renderListItem(product);
+    productList.appendChild(listItem);
+  });
+};
+
+renderProductList();
