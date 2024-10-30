292b3d7a22c42ac9a9c5a1938082a18ce21ddc0d
Ryan <ryanshaunsmith@gmail.com>
2024-10-30 20:42:11 +0000

    have form submit data to api and create a new product

--- public/script.js
+++ public/script.js
@@ -1,4 +1,36 @@
 const productList = document.querySelector(".product-list");
+const newProductSubmitButton = document.querySelector(".add-product-submit");
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  // post new product
+  addProduct();
+};
+
+const addProduct = async () => {
+  const response = await fetch("http://localhost:3001/api/products", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(gatherFormData()),
+  });
+  const data = await response.json();
+  console.log(data);
+};
+
+const gatherFormData = () => {
+  const stockNumber = document.querySelector("#stock-number").value;
+  const name = document.querySelector("#product-name").value;
+  const description = document.querySelector("#product-description").value;
+  const price = document.querySelector("#product-price").value;
+  return {
+    stockNumber,
+    name,
+    description,
+    price,
+  };
+};
+
+newProductSubmitButton.addEventListener("click", handleSubmit);
 
 const getProductData = async () => {
   const response = await fetch("http://localhost:3001/api/products");
@@ -32,3 +64,7 @@ const renderProductList = async () => {
 };
 
 renderProductList();
+
+// POST new product
+// add event listener to submit button
+// test event response
